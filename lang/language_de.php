5c279746c25c2109e9c7ce3bf491e0a0713305ab
Martin Plötz <github@hoetz.com>
2015-12-24 14:55:57 +0100

    add language file

---
+++ lang/language_de.php
@@ -0,0 +1,15 @@
+<?php
+  # Language Terms German
+  define('TITLE',       'Ramadan - Wann darf ich essen?');
+  define('KEYWORDS',    'Ramadan');
+  define('DESCRIPTION', 'Ramadan');
+  define('EAT',         'Essen');
+  define('FAST',        'Fasten');
+  define('STILL',       'noch');
+  define('DATESTRING',  'Heute ist %dayname%, der %date%. Es ist %fulltime% Uhr.');
+  define('YOUAREIN',    'Du bist in');
+  define('SUNRISE',     'Sonnenaufgang');
+  define('SUNSET',      'Sonnenuntergang');
+  define('TODAY',       'Heute');
+  define('TOMORROW',    'Morgen');
+?>
